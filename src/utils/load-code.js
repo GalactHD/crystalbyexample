016bed2a65277cc2c30ee69bb29c52fe17c3123f
GalactHD <139361517+GalactHD@users.noreply.github.com>
2025-11-21 16:35:00 -0300

    removed to lower case from example...

--- src/utils/load-code.js
+++ src/utils/load-code.js
@@ -1,16 +1,12 @@
 /**
  * Load a example from the ./public/code/* directory.
- * @param {string} example
+ * @param {string} example need to be lowercase.
  * @returns {Promise<string>}
  */
 export const loadCode = async (example) => {
-  const response = await fetch(
-    `https://crystalbyexample.pages.dev/code/${example.toLowerCase()}.cr`
-  );
+  const response = await fetch(`https://crystalbyexample.pages.dev/code/${example}.cr`);
   if (!response.ok)
     throw new Error(`Failed to load! Status: ${response.status}`);
 
   return await response.text();
 };
-
-console.log(process.env.NODE_ENV);
