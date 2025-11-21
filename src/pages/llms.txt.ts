cef499ac31f2a84e6e47cfcb04a2894d25f24c3f
GalactHD <139361517+GalactHD@users.noreply.github.com>
2025-11-21 16:10:01 -0300

    feat: pages are now mdx, and updated packages.

--- src/pages/llms.txt.ts
+++ src/pages/llms.txt.ts
@@ -6,11 +6,12 @@ export const GET: APIRoute = async () => {
   const content = `# Crystal by example\n
     ${examples
       .sort((a, b) => a.data.order - b.data.order)
-      .map((example) =>
-          `# ${example.data.title}\n
-            https://crystalbyexample.pages.dev/${example.id} \n
-            ${example.data.title} \n
-            ${example.body}`).join('\n\n')}`;
+      .map(
+        (example) => `# ${example.data.title}\n
+    https://crystalbyexample.pages.dev/${example.id}\n
+    ${example.body}`
+      )
+      .join('\n\n')}`;
 
   return new Response(content, {
     headers: { 'Content-Type': 'text/plain; charset=utf-8' },
