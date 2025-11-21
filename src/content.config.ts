59cff6f1ab995c5122b1eaea5db87e9ffe870cb2
GalactHD <139361517+GalactHD@users.noreply.github.com>
2025-11-21 15:22:17 -0300

    edit glob pattern for loading mdx

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, reference, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const examples = defineCollection({
-  loader: glob({ pattern: '**/*.{md, mdx}', base: './src/content/examples' }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/examples' }),
   schema: z.object({
     title: z.string(),
     order: z.number(),
