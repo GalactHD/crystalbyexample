1a286e8ed33fac1098656517908932b837f3748f
GalactHD <139361517+GalactHD@users.noreply.github.com>
2025-11-29 08:55:57 -0300

    refactor: llms.txt formatted

--- src/pages/llms.txt.ts
+++ src/pages/llms.txt.ts
@@ -3,15 +3,20 @@ import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async () => {
   const examples = await getCollection('examples');
-  const content = `# Crystal by example\n
-    ${examples
-      .sort((a, b) => a.data.order - b.data.order)
-      .map(
-        (example) => `# ${example.data.title}\n
-    https://crystalbyexample.pages.dev/${example.id}\n
-    ${example.body}`
-      )
-      .join('\n\n')}`;
+  const sorted = examples.sort((a, b) => a.data.order - b.data.order);
+  const sections = sorted.map((example) => {
+    return [
+      `# ${example.data.title}\n`,
+      `https://crystalbyexample.pages.dev/${example.id}`,
+      example.body?.trim(),
+    ].join('\n');
+  });
+
+  const content = [
+    '<SYSTEM>This is code examples about the Crystal Programming Language</SYSTEM>',
+    '# Crystal by example',
+    ...sections,
+  ].join('\n\n');
 
   return new Response(content, {
     headers: { 'Content-Type': 'text/plain; charset=utf-8' },
